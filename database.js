57b2484e38db238644bf36a507cca979001be1ba
Alismos <46933082+Alismos@users.noreply.github.com>
2022-03-08 14:35:09 -0500

    Delete database.js
    Delete commented files

--- database.js
+++
@@ -1,6 +0,0 @@
-/*const mongoose = require("mongoose");
-
-mongoose
-    .connect("mongodb://mongodb:27017/expresscart")
-    .then((db) => console.log("Db is connected to", db.connection.host))
-    .catch((err) => console.error(err));*/
